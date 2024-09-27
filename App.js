42b392f1b99626881b7322ab40dde0675d6e4b65
Daniel Meireles do Rego <dannryu@gmail.com>
2024-09-27 10:04:58 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
+
+export default function App() {
+
+  const [isActive, setisActive] = useState(false)
+
+
+  function handleSymbol(){
+    setisActive((oldValue:boolean) =>{
+      return !oldValue
+    })
+
+  }
+
+  return (
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+
+       <TouchableOpacity onPress={()=>{handleSymbol()}}>
+       <Image source={ isActive ? symbolOn : symbolOff }></Image>
+       </TouchableOpacity>
+
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  containerOn: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerOff:{
+    flex:1,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
